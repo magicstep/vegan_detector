32ecacc3bad2c0661b52d07ac4368db7b17dd9fc
Piotr <magicstep@gmail.com>
2023-02-10 19:22:19 +0000

    Add files via upload

--- vegan_detector.cmd
+++ vegan_detector.cmd
@@ -1,5 +1,24 @@
 @echo off
-echo    --- Vegan Detector ---
-echo How can you tell whether someone is vegan ?
+echo    --- Vegan Detector v1.0 ---
+echo. 
+echo How can you tell whether someone is vegan ???
+echo. 
 pause
 echo Don't worry. They will tell you!
+echo. 
+
+pause
+echo Search for challenge22
+echo they will help you go vegan for free
+echo it's 22 days with a team of nutritionists and a personal mentor.
+echo. 
+echo.
+echo Try some of the free challenges and watch videos
+echo Greta Thunberg https://www.youtube.com/watch?v=7WvehTbuvIo
+echo Why Vegan? https://www.youtube.com/watch?v=y2k4NHjAP84
+echo Some good resources and free support:
+echo https://vbcamp.org Vegan Bootcamp, free challenge that will take you step-by-step
+echo https://veganoutreach.org/vegan
+
+pause >nul
+exit
